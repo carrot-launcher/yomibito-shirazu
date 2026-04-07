30dcc409ef86fcf3f3070d0c2a6a317fa1e940fa
gravity-coupon <gravity.coupon@gmail.com>
2026-04-08 08:20:01 +0900

    public safe

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,22 +1,23 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 import { initializeApp } from 'firebase/app';
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 
-// ★ Firebase Console からコピーして置き換えてください
+const extra = Constants.expoConfig?.extra || {};
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDgkHl-ypOEwZjMjdp0Ny5qxn1CApB8SA8",
-  authDomain: "yomibito-shirazu-9e7fe.firebaseapp.com",
-  projectId: "yomibito-shirazu-9e7fe",
-  storageBucket: "yomibito-shirazu-9e7fe.firebasestorage.app",
-  messagingSenderId: "295815869327",
-  appId: "1:295815869327:web:882f1219cdc081ea6851fa",
-  measurementId: "G-RX7FV4PKZ0"
+  apiKey: extra.firebaseApiKey,
+  authDomain: extra.firebaseAuthDomain,
+  projectId: extra.firebaseProjectId,
+  storageBucket: extra.firebaseStorageBucket,
+  messagingSenderId: extra.firebaseMessagingSenderId,
+  appId: extra.firebaseAppId,
+  measurementId: extra.firebaseMeasurementId,
 };
 
-// ★ Google Cloud Console の OAuth 2.0 Web Client ID
-export const WEB_CLIENT_ID = "295815869327-q3ebjdo3ulaaiiker2s2og0m2kkldt9o.apps.googleusercontent.com";
+export const WEB_CLIENT_ID = extra.googleWebClientId;
 
 const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
